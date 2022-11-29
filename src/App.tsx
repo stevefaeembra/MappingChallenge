4271fc63f991d528bbb5887d3ac035994e2aa8a9
stevefaeembra <stevendkay@gmail.com>
2022-11-29 16:19:22 +0000

    feat(frontend): Added text labels
    Additional layer to show pub names
    
    IssueID #

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import 'mapbox-gl/dist/mapbox-gl.css';
 import Map from 'react-map-gl';
 import DeckGL from '@deck.gl/react/typed';
 import { Fragment } from 'react';
-import { GeoJsonLayer } from '@deck.gl/layers/typed';
+import { GeoJsonLayer, TextLayer } from '@deck.gl/layers/typed';
 import PUBS from '../data/pubs';
 
 
@@ -17,6 +17,7 @@ const INITIAL_VIEW_STATE = {
 };
 
 function App() {
+  console.log('data', PUBS);
   const layer = new GeoJsonLayer({
     id: 'geojson-layer',
     data: PUBS,
@@ -30,7 +31,26 @@ function App() {
     getFillColor: [255, 0, 0, 200],
     getPointRadius: 20,
     getLineWidth: 1,
-    getElevation: 30
+    getElevation: 30,
+    fontSettings: {sdf: true, outlineWidth: 4,},
+  });
+  const layer2 = new GeoJsonLayer({
+    id: 'text-layer',
+    data: PUBS,
+    pickable: true,
+    pointType: 'text',
+    getPosition: d => d.coordinates,
+    getText: d => d.properties.name ,
+    getPointRadius: 10,
+    getAngle: 0,
+    getTextAnchor: 'start',
+    getAlignmentBaseline: 'center',
+    textSizeScale: 0.5,
+    outlineColor: [255,0,0,255],
+    textOutlineColor: [255,0,0,255],
+    outlineWidth: 40,
+    //textOutlineWidth: 12,
+    fontSettings: {sdf: true, outlineWidth: 40,},
   });
   //console.log('pubs', PUBS);
   return (
@@ -44,7 +64,7 @@ function App() {
           <DeckGL 
             initialViewState={INITIAL_VIEW_STATE}
             controller={true}
-            layers={[layer]}
+            layers={[layer, layer2]}
           >
             <Map
               style={{width: 600, height: 400}}
