af59b3890021d2bc8c39453224e15082aac18cf7
stevefaeembra <stevendkay@gmail.com>
2022-11-29 12:15:47 +0000

    feat(frontend): Hello world map
    MVP, behold a map!
    
    IssueID #

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
 import './App.css'
 import { MAPBOX_TOKEN } from './secret.js';
+import 'mapbox-gl/dist/mapbox-gl.css';
+import Map from 'react-map-gl';
 
 function App() {
 
   return (
     <div className="App">
-      <h1>Hello {MAPBOX_TOKEN}</h1>
+      <h1>Hello Map Challenge</h1>
+      <Map
+        initialViewState={{
+          longitude: -122.4,
+          latitude: 37.8,
+          zoom: 14
+        }}
+        style={{width: 600, height: 400}}
+        mapStyle="mapbox://styles/mapbox/streets-v9"
+        mapboxAccessToken={MAPBOX_TOKEN}
+      />
     </div>
   )
 }
