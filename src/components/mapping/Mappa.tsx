5dea492648668e7a3889b0867223213dcb45bb5d
stevefaeembra <stevendkay@gmail.com>
2022-12-02 12:28:10 +0000

    feat(frontend): Fix crash when you hover over building
    Added elvis to make tooltip code more reliable
    
    IssueID #

--- src/components/mapping/Mappa.tsx
+++ src/components/mapping/Mappa.tsx
@@ -59,7 +59,7 @@ function Mappa(props: Props) {
             initialViewState={viewState}
             controller={true}
             layers={props.layers}
-            getTooltip={({object}) => object && (object.properties.name || object.properties.ref || "No data")} 
+            getTooltip={({object}) => object && (object?.properties?.name || object?.properties?.ref || "No data")} 
             layerFilter={ ({layer, viewport}) => {
               if (viewport.zoom<15 && layer.id === 'Pubs Labels') {
                 // hide labels when zoomed out
